8bd755eeb48c170706a0cd01f11fc8e446edb118
Jason Vandeyar <jasonvandeyar55@gmail.com>
2021-01-09 18:39:46 +0200

    Updated styling on symptom checklist screen\

--- lib/screens/symptom_checklist_screen.dart
+++ lib/screens/symptom_checklist_screen.dart
@@ -1,58 +1,59 @@
+import 'package:covid_apptitude/components/template_widgets.dart';
 import 'package:covid_apptitude/screens/quiz_loading_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:covid_apptitude/components/template_widgets.dart';
 
 class SymptomChecklistScreen extends StatelessWidget {
-
   static String id = 'symptom_checklist_screen';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.red,
-            title: Text(
-              'Covid Apptitude',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24.0,
-              ),
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(
+            'Covid Apptitude',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24.0,
             ),
           ),
-          body: Column(
-              children: [
-                DetailsHeading('Are you experiencing?'),
-                CheckboxWidget(),
-                SizedBox(height: 25.0),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20.0),
-                  child: OutlinedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, QuizLoadingScreen.id);
-                    },
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(
-                        color: Colors.red,
-                        width: 1.5,
-                      ),
-                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        'SUBMIT',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 16.0,
-                        ),
-                      ),
+        ),
+        body: Column(
+          children: [
+            DetailsHeading('Are you experiencing?'),
+            SizedBox(height: 35.0),
+            CheckboxWidget(),
+            SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, QuizLoadingScreen.id);
+                },
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(
+                    color: Colors.red,
+                    width: 1.5,
+                  ),
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    'SUBMIT',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 16.0,
                     ),
                   ),
                 ),
-              ],
-          ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -64,28 +65,25 @@ class CheckboxWidget extends StatefulWidget {
 }
 
 class CheckboxWidgetState extends State {
-
   Map<String, bool> symptoms = {
-    'Fever or chills' : false,
-    'Cough' : false,
-    'Shortness of breath' : false,
-    'Fatigue' : false,
-    'Muscle or body aches' : false,
-    'Headache' : false,
-    'Loss of taste or smell' : false,
-    'Sore throat' : false,
-    'Congestion or runny nose' : false,
-    'Nausea or vomiting' : false,
-    'Diarrhea' : false,
+    'Fever or chills': false,
+    'Cough': false,
+    'Shortness of breath': false,
+    'Fatigue': false,
+    'Muscle or body aches': false,
+    'Headache': false,
+    'Loss of taste or smell': false,
+    'Sore throat': false,
+    'Congestion or runny nose': false,
+    'Nausea or vomiting': false,
+    'Diarrhea': false,
   };
 
   var holder_1 = [];
 
-  getItems(){
-
+  getItems() {
     symptoms.forEach((key, value) {
-      if(value == true)
-      {
+      if (value == true) {
         holder_1.add(key);
       }
     });
@@ -97,7 +95,7 @@ class CheckboxWidgetState extends State {
   Widget build(BuildContext context) {
     return Expanded(
       flex: 12,
-      child : ListView(
+      child: ListView(
         scrollDirection: Axis.vertical,
         children: symptoms.keys.map((String key) {
           return new CheckboxListTile(
